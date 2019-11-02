6344a9744ded6279c47f4ce3b324457e6c43ab05
Nightmayr <umayr23@gmail.com>
2019-11-02 16:26:52 +0000

    added check for installed java

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -12,11 +12,14 @@ if [ "$EUID" -ne 0 ]
 fi
 
 if [ ! -d '/var/lib/jenkins' ]; then
+    if [ ! -x "$(command -v java)" ]; then
+        apt-get update
+        apt-get install -y openjdk-8-jre
     if [ $DISTRO == "ubuntu" ] || [ $DISTRO == "debian" ] || [ $DISTRO == "raspbian" ]; then
         wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
         sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
         sudo apt-get update
-        sudo apt-get install jenkins
+        sudo apt-get install -y jenkins
     fi
 
     systemctl start jenkins

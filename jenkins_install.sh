808696e3c0d27f4ec05902a83e8508ac38bb2bd2
Nightmayr <umayr23@gmail.com>
2019-11-02 13:31:33 +0000

    created jenkins install script

---
+++ jenkins_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export DISTRO=$(sed -n '/\bID\b/p' /etc/os-release | awk -F= '/^ID/{print $2}' | tr -d '"')
+
+if [ "$EUID" -ne 0 ]
+    then echo "Please run script as root"
+    exit
+fi
+
+if [ ! -d '/var/lib/jenkins' ]; then
+    if [ $DISTRO == "ubuntu" || $DISTRO == "debian" || $DISTRO == "raspbian" ]; then
+        wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+        sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+        sudo apt-get update
+        sudo apt-get install jenkins
+    fi
+
+    systemctl start jenkins
+    jenkins_cli_setup
+fi
+
+function jenkins_cli_setup {
+    curl localhost:/jnlpJars/jenkins-cli.jar -o jenkins-cli.jar
+}

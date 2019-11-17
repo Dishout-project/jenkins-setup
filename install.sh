e9b879a3147dbcc9cb61cf67a4a1239044a8ce51
Nightmayr <umayr23@gmail.com>
2019-11-17 22:57:50 +0000

    changed permission of ssh directory in JENKINS_HOME

--- install.sh
+++ install.sh
@@ -47,7 +47,8 @@ function generate_ssh_keys() {
     echo "Creating ssh keys"
     mkdir -p $JENKINS_HOME/.ssh
     chmod 700 $JENKINS_HOME/.ssh
-    su - jenkins -c "ssh-keygen -N \"\" -f $JENKINS_HOME/.ssh/id_rsa"
+    chown -R jenkins:jenkins $JENKINS_HOME/.ssh
+    su - jenkins -c "ssh-keygen -N \"\" -f $JENKINS_HOME/.ssh/id_rsa" 2>/dev/null
 }
 
 function install_plugins () {
